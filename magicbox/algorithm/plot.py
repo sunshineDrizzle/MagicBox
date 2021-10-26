9ffebb56beba75451d17b131c721d8473ab2a7d0
sunshineDrizzle <954830460@qq.com>
2021-10-25 22:21:20 -0400

    MidpointNormalize

--- magicbox/algorithm/plot.py
+++ magicbox/algorithm/plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, colors
 
 
 def show_bar_value(rects, val_fmt='', ax=None):
@@ -173,3 +173,16 @@ class VlineMoverPlotter(object):
 
     def _on_clicked(self, event):
         pass
+
+
+class MidpointNormalize(colors.Normalize):
+    # https://matplotlib.org/stable/gallery/userdemo/colormap_normalizations.html
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        super().__init__(vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
