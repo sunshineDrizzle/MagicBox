0d0778037bdb7dd8d000b2258391e0f959e3e8c6
sunshineDrizzle <954830460@qq.com>
2022-04-22 04:05:07 -0400

    NF: plot_polyfit

--- magicbox/algorithm/plot.py
+++ magicbox/algorithm/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
 from matplotlib import pyplot as plt, colors
 
 
@@ -603,6 +604,64 @@ def plot_line(y, n_row=1, n_col=1, figsize=None, yerr=None, x=None,
     plot_axes(fig, axes, n_ax, xlabel=xlabel, xtick=xtick, xticklabel=xticklabel,
               rotate_xticklabel=rotate_xticklabel, ylabel=ylabel, ylim=ylim,
               title=title, mode=mode)
+
+
+def plot_polyfit(x, y, deg, scoring='r', scatter_plot=True,
+                 color=None, label=None, s=None, c=None, marker=None,
+                 n_sample=100, ax=None):
+    """
+    Parameters
+    ----------
+    x : ndarray, shape (M,)
+        x-coordinates of the M sample points ``(x[i], y[i])``.
+    y : ndarray, shape (M,)
+        y-coordinates of the M sample points ``(x[i], y[i])``.
+    deg : int
+        Degree of the fitting polynomial
+    scoring : str
+        a method used to evaluate the fit effect
+    scatter_plot : bool
+        If True, do scatter plot
+    color : parameters for plt.plot
+    label : parameters for plt.plot and plt.scatter
+    s, c, marker : parameters for plt.scatter
+    n_sample : positive integer
+        the number of points used to plot fitted curve
+    ax : matplotlib axis
+
+    References
+    ----------
+    1. https://blog.csdn.net/fffsolomon/article/details/104831050
+    """
+    # fit and construct polynomial
+    coefs = np.polyfit(x, y, deg)
+    polynomial = np.poly1d(coefs)
+    print('polynomial:\n', polynomial)
+
+    # scoring
+    y_pred = polynomial(x)
+    if scoring == 'r':
+        score = pearsonr(y, y_pred)
+    elif scoring == 'r2_score':
+        score = r2_score(y, y_pred)
+    else:
+        raise ValueError("Not supported scoring:", scoring)
+    print('\nscore:', score)
+
+    if ax is None:
+        _, ax = plt.subplots()
+
+    # plot scatter
+    if scatter_plot:
+        ax.scatter(x, y, c=c, label=label, s=s, marker=marker)
+
+    # plot fitted curve
+    x_min, x_max = np.min(x), np.max(x)
+    x_plot = np.linspace(x_min, x_max, n_sample)
+    y_plot = polynomial(x_plot)
+    ax.plot(x_plot, y_plot, color=color, label=label)
+
+    return ax
 
 
 class VlineMover(object):
